c5b90fd14065d61002acf442dff3d84178fdb45d
yangguo <yangguo@outlook.com>
2018-02-26 21:33:23 +0800

    first commit

---
+++ src/main/java/info/yangguo/classloader/SpringBootLoaderApplication.java
@@ -0,0 +1,12 @@
+package info.yangguo.classloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoaderApplication.class, args);
+	}
+}
